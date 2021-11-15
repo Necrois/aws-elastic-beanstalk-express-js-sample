7e65d50d3942b14a7b413f45f607c16fd09370e7
Joachim <joachim.amundsen@gmail.com>
2021-11-15 15:28:55 +0100

    Line 5 Text change for pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('SpaceX is far superior to blue origins! '));
+app.get('/', (req, res) => res.send('Pipeline Test 1 Hello World ? '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
